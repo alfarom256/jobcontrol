7da0a5402c9f75a82bc898a73799829be0903bbb
alfarom256 <alfarom256@gmail.com>
2020-03-29 19:35:29 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate getopts;
+/*
+args: 
+-p 1,2,3,4,5
+    process IDs to interact with
+-n <bytes_per_second>
+    restrict network to N bytes per second
+-c <100.0 - 0.01>
+    Max CPU percenta
+-q 
+    quiet
+-d 
+    debug
+
+
+*/
+fn main(){
+    println!("woooo!");
+}
