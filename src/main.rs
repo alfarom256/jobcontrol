09cba707d429d29025cd826f6d0560f70d6a61d5
alfarom256 <alfarom256@gmail.com>
2020-03-29 19:58:00 -0400

    adding initial command line arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate getopts;
+use getopts::Options;
+
 /*
 args: 
 -p 1,2,3,4,5
@@ -11,9 +13,37 @@ args:
     quiet
 -d 
     debug
-
-
 */
+
+use std::env;
+
+fn print_help(prog : &str, opts: Options){
+    let fmt = format!("Usage: {} -p/-pids 1,2,3,4 -c 0.01 -n", prog);
+    print!("{}", opts.usage(&fmt));
+}
+
 fn main(){
+    //https://www.cs.brandeis.edu/~cs146a/rust/rustbyexample-02-21-2015/arg/getopts.html
+    let args: Vec<String> = env::args().map(|x| x.to_string()).collect();
+    let ref prog = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("p", "pids", "PID list to interact with", "4,1013,139213,400");
+    opts.optopt("c", "cpu", "max CPU usage for all processes (must be between 100.0 and 0.01)", "0.01");
+    opts.optopt("n", "netrate", "attempt to set the bytes per second of network traffic for all processes ( >= W10, Server 2016 ONLY )", "50");
+
+    opts.optflag("q", "quiet", "do not display output");
+    opts.optflag("d", "debug", "display debug output");
+    opts.optflag("h", "help", "display help and usage");
+    
+    let matches = match opts.parse(args){
+        Ok(m) => {m},
+        Err(f) => { panic!(f.to_string()) }
+    };
+    
+    if matches.opt_present("h") {
+        print_help(prog, opts)
+    }
+    
     println!("woooo!");
 }
