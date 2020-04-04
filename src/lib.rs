9e01342d05f594e1f5db66810e2a66f4612d4497
alfarom256 <alfarom256@gmail.com>
2020-04-04 19:00:39 -0400

    adding winapi crate and building out initial functions needed

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,20 @@
+extern crate winapi;
+use std::io::Error;
+use self::winapi::um::winnt::{HANDLE};
+use self::winapi::shared::minwindef::BOOL;
+
+
 pub fn assign_and_process_job(pids: Vec<u32>, net_ctl : u32, cpu_pct : f32, dbg : bool) -> Result<u32, &'static str> {
     println!("d00t!");
     Ok(1)
 }
+
+fn open_process() -> Result<HANDLE, Error>{
+    Ok(std::ptr::null_mut())
+}
+
+fn is_proc_in_job(){}
+
+fn create_job_object(){}
+
+fn assign_proct_to_job_object(){}
