eac11adf8918b06b4be79bb8bb535f5ea0aebe01
alfarom256 <alfarom256@gmail.com>
2020-04-05 01:01:00 -0400

    adding initial CreateJobObjectA function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 extern crate winapi;
 use std::io::Error;
+use std::ffi::CString;
 use self::winapi::um::winnt::{HANDLE, PROCESS_SET_QUOTA, PROCESS_TERMINATE, PROCESS_QUERY_LIMITED_INFORMATION};
 use self::winapi::um::processthreadsapi::{OpenProcess};
 use self::winapi::um::jobapi::{IsProcessInJob};
+use self::winapi::um::jobapi2::{CreateJobObjectW};
+use self::winapi::um::winbase::{CreateJobObjectA};
 use self::winapi::shared::minwindef::{BOOL, TRUE, FALSE};
+
 
 
 
@@ -25,8 +29,14 @@ pub fn assign_and_process_job(pids: Vec<u32>, net_ctl : u32, cpu_pct : f32, dbg 
             println!("Process already in job");
             return Err(Error::last_os_error())
         } 
-        println!("Process not in job...")
+        println!("Process not in job...");
     }
+    // create the job object
+    println!("Creating job object");
+    let h_job : HANDLE = match create_job_object(){
+        Ok(x) => x,
+        Err(e) => {return Err(e)}
+    };
 
     Ok(1)
 }
@@ -51,7 +61,18 @@ fn is_proc_in_job(h_proc : HANDLE) -> BOOL{
 }
 
 fn create_job_object() -> Result<HANDLE, Error>{
-    Ok(std::ptr::null_mut())
+    let lp_job_name = CString::new("FUG").unwrap();
+    let h_job : HANDLE;
+    unsafe {
+        h_job = CreateJobObjectA(std::ptr::null_mut(), lp_job_name.as_ptr());
+    }
+    if h_job == std::ptr::null_mut(){
+        return Err(Error::last_os_error());
+    }
+    Ok(h_job)
 }
 
-fn assign_proct_to_job_object(){}
+fn assign_proc_to_job_object(h_job : HANDLE, h_proc : HANDLE) -> Result<BOOL, Error>{
+
+    Ok(1)
+}
