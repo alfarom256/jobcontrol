4d69e626a0bc28206bbc715ae07135fb6a3e4b02
alfarom256 <alfarom256@gmail.com>
2020-04-05 15:31:56 -0400

    adding handle closing before error is returned

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,31 @@ use std::ffi::CString;
 use self::winapi::um::winnt::{HANDLE, PROCESS_SET_QUOTA, PROCESS_TERMINATE, PROCESS_QUERY_LIMITED_INFORMATION};
 use self::winapi::um::processthreadsapi::{OpenProcess};
 use self::winapi::um::jobapi::{IsProcessInJob};
-use self::winapi::um::jobapi2::{CreateJobObjectW, AssignProcessToJobObject};
+use self::winapi::um::jobapi2::{AssignProcessToJobObject};
 use self::winapi::um::winbase::{CreateJobObjectA};
+use self::winapi::um::handleapi::{CloseHandle};
 use self::winapi::shared::minwindef::{BOOL, TRUE, FALSE};
 
+/*
+todo: 
 
+Consideration needs to be made to close handles to all processes before returning an error.
+Further work needs to be done to sort out what *actually* 
+
+*/
 
 
 pub fn assign_and_process_job(pids: Vec<u32>, net_ctl : u32, cpu_pct : f32, dbg : bool) -> Result<u32, Error> {
+    
+
+
     let mut v_proc_handles : Vec<HANDLE> = Vec::new();
     // open all of the processes and get their respective handles
     for pid in pids.into_iter(){
         let h_proc = match open_process(pid){
             Ok(x) => x,
-            // if we can't open a process given in the args, return an error
-            Err(x) => return Err(x)
+            // if we can't open a process given in the args, return an error after closing all the handles
+            Err(x) => {close_handle_vec(v_proc_handles); return Err(x)}
         };
         println!("got handle for PID {}", pid);
         v_proc_handles.push(h_proc);
@@ -27,6 +37,7 @@ pub fn assign_and_process_job(pids: Vec<u32>, net_ctl : u32, cpu_pct : f32, dbg 
     for h_proc in &v_proc_handles {
         if is_proc_in_job(h_proc) == FALSE{
             println!("Process already in job");
+            close_handle_vec(v_proc_handles);
             return Err(Error::last_os_error())
         } 
         println!("Process not in job...");
@@ -48,6 +59,20 @@ pub fn assign_and_process_job(pids: Vec<u32>, net_ctl : u32, cpu_pct : f32, dbg 
     }
 
     Ok(1)
+}
+
+fn close_handle(h : &HANDLE) -> BOOL{
+    let b_res : BOOL;
+    unsafe {
+        b_res = CloseHandle(*h);
+    };
+    b_res
+}
+
+fn close_handle_vec(v_handles : Vec<HANDLE>) {
+    for h in &v_handles {
+        close_handle(h);
+    }
 }
 
 fn open_process(pid : u32) -> Result<HANDLE, Error>{
